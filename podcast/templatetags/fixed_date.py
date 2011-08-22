40a05c0e1e880efbb6de499a42f7fb42011d6672
Bram Duvigneau <bram@bramd.nl>
2011-08-22 16:09:42 +0200

    Hopefully fixed the RFC date/time issues...

---
+++ podcast/templatetags/fixed_date.py
@@ -0,0 +1,29 @@
+from django.template import Library
+from django.conf import settings
+from podcast.utils.dateformat import format, time_format
+
+register = Library()
+
+def fixed_date(value, arg=None):
+    """Formats a date according to the given format."""
+    if not value:
+        return u''
+    if arg is None:
+        arg = settings.DATE_FORMAT
+    try:
+        return format(value, arg)
+    except AttributeError:
+        return ''
+date.is_safe = False
+
+def fixed_time(value, arg=None):
+    """Formats a time according to the given format."""
+    if value in (None, u''):
+        return u''
+    if arg is None:
+        arg = settings.TIME_FORMAT
+    try:
+        return time_format(value, arg)
+    except AttributeError:
+        return ''
+time.is_safe = False
